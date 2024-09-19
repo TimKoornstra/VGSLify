982fcfc523f99015716d5b575c4b3e88714d66cf
Tim Koornstra <tim.koornstra@gmail.com>
2024-09-19 16:18:07 +0200

    Bump to 0.12.0

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'VGSLify'
 copyright = '2024, Tim Koornstra'
 author = 'Tim Koornstra'
-release = '0.11.0'
+release = '0.12.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = "0.11.0"
+version = "0.12.0"
 
 
 # Function to write the version to a _version.py file
