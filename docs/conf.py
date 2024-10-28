cea67b34195b2aa1339cbec2f09ad4c680d34f8a
Tim Koornstra <tim.koornstra@gmail.com>
2024-10-28 16:03:44 +0100

    Release 0.13.2

--- docs/conf.py
+++ docs/conf.py
@@ -13,7 +13,7 @@ sys.path.insert(0, os.path.abspath('..'))
 project = 'VGSLify'
 copyright = '2024, Tim Koornstra'
 author = 'Tim Koornstra'
-release = '0.13.0'
+release = '0.13.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 import os
 
-version = "0.13.1"
+version = "0.13.2"
 
 
 # Function to write the version to a _version.py file
