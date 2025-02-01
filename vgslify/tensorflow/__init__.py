7980468c6d3d9475e05dffc4da1f514ea7a10054
Tim Koornstra <tim.koornstra@gmail.com>
2025-02-01 20:09:54 +0100

    Extra useful imports

---
+++ vgslify/tensorflow/__init__.py
@@ -0,0 +1,6 @@
+from .layers import TensorFlowLayerFactory, register_custom_layer
+
+__all__ = [
+    "TensorFlowLayerFactory",
+    "register_custom_layer",
+]
---
+++ vgslify/torch/__init__.py
@@ -0,0 +1,6 @@
+from .layers import TorchLayerFactory, register_custom_layer
+
+__all__ = [
+    "TorchLayerFactory",
+    "register_custom_layer",
+]
